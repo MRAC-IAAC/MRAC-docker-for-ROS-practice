2d4abb71346f8bd3c34b201e335d785b3c8cf37b
huanyu <91276231+HuanyuL@users.noreply.github.com>
2024-11-03 13:57:32 +0100

    update examples

--- examples/ros2_intro/.docker/run_user_nvidia.sh
+++ examples/ros2_intro/.docker/run_user_nvidia.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-echo -e "Starting up ros_intro container \n >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+echo -e "Starting up ros2_intro container \n >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 echo -e "This container will access to the users home directory and log in as the user with their password and x sever access.\nYou will not own the workspace though, use sudo chown -R $USER /dev_ws"
-echo -e "Source the workspace with source devel/setup.bash"
+echo -e "Source the workspace with source install/setup.bash"
 
 docker run -it --privileged \
     --user=$(id -u $USER):$(id -g $USER) \
@@ -19,4 +19,5 @@ docker run -it --privileged \
     --volume="/dev:/dev" \
     --net=host \
     --cap-add=sys_nice \
-    ros_intro:latest
+    --gpus 'all,"capabilities=compute,display,graphics,utility,video"' \
+    ros2_intro:latest
--- examples/ros_intro/.docker/setup.bash
+++
@@ -1,2 +0,0 @@
-source "/opt/ros/noetic/setup.bash"
-source "/dev_ws/devel/setup.bash"
